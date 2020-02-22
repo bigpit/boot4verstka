6232fde58ae187268b970dff4c2c0cad29bb1982
bigpit <30524280+bigpit@users.noreply.github.com>
2020-02-22 23:40:30 +0200

    Начато работу раздела блог

--- js/main.js
+++ js/main.js
@@ -18,7 +18,10 @@ $(document).ready(function(){
  $('.multiple-items').slick({
    infinite: true,
    slidesToShow: 3,
-   slidesToScroll: 3
+   slidesToScroll: 3,
+   arrows:false,
+   dots:true,
+   dotsClass:'dots-style'
  });
 
 
